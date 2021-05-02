4cac51c4ab1d0d17bb55edbf3bf0ff49cbaa1e50
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2021-05-02 10:26:52 +0100

    Created Project

---
+++ src/test/java/com/ed2nd/mywallet/MywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed2nd.mywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
